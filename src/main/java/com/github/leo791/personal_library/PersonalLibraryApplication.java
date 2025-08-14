685fc8856830fbf743c0cc6daef38e461c91b7e7
Leo791 <leonormp.alves@gmail.com>
2025-08-14 10:26:08 +0100

    initialized setup with start.spring.io/

---
+++ src/main/java/com/github/leo791/personal_library/PersonalLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.github.leo791.personal_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/leo791/personal_library/PersonalLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leo791.personal_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
