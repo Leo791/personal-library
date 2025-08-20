0c20a0506f5c9645a3e19a7d26d1ad605e4bfed8
Leo791 <leonormp.alves@gmail.com>
2025-08-20 12:09:59 +0100

    Adding JaCoCo plugin

--- src/test/java/com/github/leo791/personal_library/PersonalLibraryApplicationTests.java
+++ src/test/java/com/github/leo791/personal_library/PersonalLibraryApplicationTests.java
@@ -3,7 +3,7 @@ package com.github.leo791.personal_library;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class PersonalLibraryApplicationTests {
 
 	@Test
